1a9bcaebbe7a099409b61cf1fb1039fa8215e070
Nocxxecru <nicohoarau74@gmail.com>
2020-02-26 09:36:56 +0100

    Récupère les animes

---
+++ anime.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
+  <link rel="stylesheet" href="./css/style.css">
+  <title></title>
+</head>
+
+<body>
+  <div id="anime"></div>
+
+  <!--    IMPORT BOOTSRAP   -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/i18n/defaults-*.min.js"></script>
+
+  <!--    IMPORT JQUERY   -->
+  <script src="https://code.jquery.com/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <script src="./js/anime.js"></script>
+</body>
+
+</html>
---
+++ js/anime.js
@@ -0,0 +1,47 @@
+$(document).ready(() => {
+  GetAnimeData();
+});
+
+function GetAnimeData() {
+  let url_string = document.URL;
+  let url = new URL(url_string);
+  let idAnime = url.searchParams.get("idAnime");
+
+  $.ajax({
+    type: "post",
+    url: "./lib/getAnime.php",
+    data: { idAnime: idAnime },
+    dataType: "json",
+    success: data => {
+      ShowAnime(data);
+    }
+  });
+}
+
+function ShowAnime(data) {
+  let html =
+    '<div class="container">' +
+      '<div class="row">' +
+        '<div class="col col-sm-auto">' +
+          '<img src="' + data.cover + '" alt="anime cover" height="200" width="150">' +
+        "</div>" +
+        '<div class="col col-lg-10">' +
+          "<br>" +
+          data.descritpion +
+        "</div>" +
+      "</div>" +
+      "<br>" +
+      "<table>" +
+        "<tr>" +
+          "<th>My Score</th>" +
+          "<th>Score Average</th>" +
+        "</tr>" +
+        "<tr>" +
+          "<td>" + data.userScore + "</td>" +
+          "<td>" + data.avgScore + "</td>" +
+        "</tr>" +
+      "</table>" +
+    "</div>";
+
+    $('#anime').html(html);
+}
